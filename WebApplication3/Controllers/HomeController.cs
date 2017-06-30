031f4b2abcc9b21bbbd01e0191e5a334a54e7c52
Valerii <valeriikozhevnikov@gmail.com>
2017-06-30 22:33:19 +0300

    Site for registration

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int h = DateTime.Now.Hour;
+            ViewBag.Hello = h < 12 ? "Good morning, " : "Good afternoon, ";
+            return View();
+        }
+
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+    }
+}
